36f0cd89a5797b219c0d4f851ab5bc67fe2c4f1a
vedatsen27 <91390977+vedatsen27@users.noreply.github.com>
2021-09-25 22:54:22 +0300

    hesapmakinesi.py
    kısa ve basit bir hesap makinesi

---
+++ REAEDM.py
@@ -0,0 +1,31 @@
+# wep-developer
+print("operasyon")
+print("1 : topla")
+print("2 : çıkar")
+print("3 : çarp")
+print("4 : böl")
+
+sayı = int(input("bir sayı giriniz "))
+sayi1 = 1
+sayi2 = 2
+sayi3 = 3
+sayi4 = 4
+if sayı==1:
+    ilk1 = int(input("sayi giriniz"))
+    ilk2 = int(input("sayi giriniz"))
+    print(ilk1 + ilk2)
+elif sayı==2:
+    ikinci1 = int(input("sayi giriniz"))
+    ikinci2 = int(input("sayi giriniz"))
+    print(ikinci1 - ikinci2)
+elif sayı==3:
+    üç1 = int(input("sayi giriniz"))
+    üç2 = int(input("sayi giriniz"))
+    print(üç1 * üç2)
+elif sayı==4:
+    dört1 = int(input("sayi giriniz"))
+    dört2 = int(input("sayi giriniz"))
+    print(dört1 / dört2)   
+  
+      
+      
